5e8c822650dcd4620a31d8e1a7b679fedbf9c7c3
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-11-30 17:25:27 +0100

    Refactor Card and PostList components

--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -7,7 +7,16 @@ export default function card(props) {
       <div className="p-4 text-left ">
         <h1 className="md:text-2xl">{props.title}</h1>
         <p className="my-4 text-sm md:text-base">{props.content}</p>
-        <span>{props.tags}</span>
+        <ul>
+          {props.tags.map((tag, i) => (
+            <li
+              key={i}
+              className="mr-2 inline-block rounded-lg bg-gray-200 p-1 text-xs md:text-sm"
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
         <span>{props.published}</span>
         <br />
         <button className="mt-4 bg-orange-400 p-2 text-sm text-white">
--- src/components/partials/PostList.jsx
+++ src/components/partials/PostList.jsx
@@ -2,14 +2,17 @@ import posts from '../../data/posts';
 import Card from './Card';
 
 export default function PostList() {
-  return posts.map((post) => (
-    <Card
-      key={post.id}
-      image={post.image}
-      title={post.title}
-      content={post.content}
-      tags={post.tags}
-      published={post.published}
-    />
-  ));
+  return posts.map(
+    (post) =>
+      post.published && (
+        <Card
+          key={post.id}
+          image={post.image}
+          title={post.title}
+          content={post.content}
+          tags={post.tags}
+          published={post.published}
+        />
+      ),
+  );
 }
