618d92b1299cf68c14696ad44cd6de41b830e30e
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-11-30 18:04:54 +0100

    tags colors

--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -1,3 +1,26 @@
+function TagColor({ tags }) {
+  const toReturn = [];
+  const colorsTag = {
+    php: 'bg-red-400',
+    css: 'bg-orange-400',
+    js: 'bg-yellow-400',
+    html: 'bg-blue-400',
+  };
+  tags.forEach((tag, i) => {
+    const color = colorsTag[tag] ?? 'bg-gray-400';
+
+    toReturn.push(
+      <li
+        key={i}
+        className={`mr-2 inline-block rounded-lg p-1 text-xs md:text-sm ${color}`}
+      >
+        {tag}
+      </li>,
+    );
+  });
+  return toReturn;
+}
+
 export default function card(props) {
   return (
     <div className="max-w-[600px] rounded-lg bg-white">
@@ -8,14 +31,7 @@ export default function card(props) {
         <h1 className="md:text-2xl">{props.title}</h1>
         <p className="my-4 text-sm md:text-base">{props.content}</p>
         <ul>
-          {props.tags.map((tag, i) => (
-            <li
-              key={i}
-              className="mr-2 inline-block rounded-lg bg-gray-200 p-1 text-xs md:text-sm"
-            >
-              {tag}
-            </li>
-          ))}
+          <TagColor tags={props.tags} />
         </ul>
         <span>{props.published}</span>
         <br />
